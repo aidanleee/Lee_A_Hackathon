e8c404d0718af07fbd44eb30c823742fc4815f31
Aidan Lee <Aidan@Aidans-MacBook-Pro.local>
2019-03-08 12:50:47 -0500

    add home route

--- js/components/HomeComponent.js
+++ js/components/HomeComponent.js
@@ -1,10 +1,20 @@
 export default {
-    templat: `
-
+    template: `
+    <div id="home-container" class="row">
+        <div id="logo" class="col-10 offset-1 col-lg-8 offset-lg-2">
+            <img src="images/logo-white.svg" alt="Logo">
+        </div>
+        <div id="title" class="col-12">
+            <h2>Find Yourself at Home</h2>
+        </div>
+        <router-link :to="{ path: '/signup' }" class="button">Sign Up</router-link>
+    </div>
     `,
 
     data() {
-
+        return {
+            message: "hello from the users page",
+        } 
     },
 
     methods: {
